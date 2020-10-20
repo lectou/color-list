5a3b2b8ef1c6b7e42969894bacc854278a12124d
Портянко Олександр <lara_57_pr@outlook.com>
2020-10-20 14:42:53 +0300

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import {HashRouter, BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-  <HashRouter basename={process.eny.PUBLIC_URL}>
+  <BrowserRouter basename={process.eny.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
